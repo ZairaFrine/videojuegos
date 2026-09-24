07464a99382e3d6e8eeddbd214c6ded0b44dcec6
Zaira Acosta <zaifrine@gmail.com>
2026-09-24 01:38:54 +0000

    estructura SRC

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const videojuegosRoutes = require("./routes/videojuegos");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/videojuegos", videojuegosRoutes);
+
+app.listen(3000, () => {
+    console.log("Servidor ejecutándose en http://localhost:3000");
+});

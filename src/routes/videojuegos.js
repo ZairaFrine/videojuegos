05e49209a12b2c25640becf2bc00c608a169fc49
Zaira Acosta <zaifrine@gmail.com>
2026-09-24 02:53:11 +0000

    Consulta completa de tabla principal

--- src/routes/videojuegos.js
+++ src/routes/videojuegos.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
 
     const [rows] = await connection.query(
-        "SELECT * FROM bd_videojuegos"
+        "SELECT VG.idVid AS 'ID', VG.nmVid AS 'Nombre', VG.descVid AS 'Descripción', VG.dateVid AS 'Lanzamiento', catP.nmPlat AS 'Plataforma'  FROM bd_videojuegos VG LEFT JOIN int_vidplat Pl ON VG.idVid = Pl.idVid LEFT JOIN cat_plataforma catP ON Pl.idPlat = catP.idPlat"
     );
 
     res.json(rows);
